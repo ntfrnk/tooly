ad82c0b25072121913cc3423788221afd808973f
Franco D. Ocaranza <netfrank777@gmail.com>
2022-03-21 23:41:03 -0300

    Tooly / Ocaranza

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/App.scss'
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="container flex">
+          <div className="logo">
+            tooly<span>.com</span>
+          </div>
+          <NavBar />
+        </div>
       </header>
     </div>
   );
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import './NavBar.scss'
+
+const NavBar = () => {
+
+    return (
+        <nav>
+            <a href="#">Contrucción</a>
+            <a href="#">Electricidad</a>
+            <a href="#">Carpintería</a>
+            <a href="#">Plomería</a>
+        </nav>
+    )
+}
+
+export default NavBar
